9ffc2f5205f32e91de679eef1d9cdc5c463cd484
vgnaruto <caurnt@gmail.com>
2018-03-02 17:52:38 +0700

    Layout + Calculator
    -Ngebenerin layout
    -Cara menghitung dibenerin lagi

--- Calculator/app/src/main/java/com/example/android/calculator/Calculator.java
+++ Calculator/app/src/main/java/com/example/android/calculator/Calculator.java
@@ -1,5 +1,9 @@
 package com.example.android.calculator;
 
+import android.util.Log;
+
+import java.util.LinkedList;
+
 /**
  * Kelas ini yang akan mengatur operasi matematika
  * Operasi dasar : +,-,*,/
@@ -7,7 +11,7 @@ package com.example.android.calculator;
  */
 
 public class Calculator {
-    //TODO LENGKAPIN KODE DI BAWAH INI( UNTUK ARIO)
+    public static double result = 0;
     public static double calculate(String operand1, String operand2, String operator){
         double op1 = Double.parseDouble(operand1);
         double op2 = Double.parseDouble(operand2);
@@ -31,5 +35,48 @@ public class Calculator {
                 break;
         }
         return result;
+    }
+    public static boolean getAnswer(LinkedList<CustomImageView> ll){
+        result = 0;
+        int jumlahOperator = 0;
+        int jumlahOperand = 0;
+        String op1 = "",op2 = "",ot = "";
+        boolean bagianOperand = true;
+        while(!ll.isEmpty()){
+            CustomImageView civ = ll.removeFirst();
+//            if(civ ==null){
+//                Log.d("CALCULATE","NULL");
+//            }else{
+//                Log.d("CALCULATE",civ.getText());
+//            }
+            if(civ != null){
+                if(!civ.getIsOperator() && bagianOperand){
+                    if(op1.equalsIgnoreCase("")){
+                        op1 = civ.getText();
+                    }else{
+                        op2 = civ.getText();
+                        op1 = ""+Calculator.calculate(op1,op2,ot);
+                        result = Double.parseDouble(op1);
+                        op2 = "";
+                        ot = "";
+                    }
+                    bagianOperand = false;
+                    jumlahOperand ++;
+                }else if(civ.getIsOperator() && !bagianOperand){
+                    ot = civ.getText();
+                    jumlahOperator++;
+                    bagianOperand = true;
+                }else{
+                    return false;
+                }
+            }
+        }
+        if(jumlahOperand == 1 && jumlahOperator == 0){
+            return false;
+        }else if(jumlahOperand - jumlahOperator != 1){
+            return false;
+        }else {
+            return true;
+        }
     }
 }
--- Calculator/app/src/main/java/com/example/android/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/android/calculator/MainActivity.java
@@ -8,6 +8,7 @@ import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,7 +22,7 @@ import android.widget.Toast;
 
 import java.util.LinkedList;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
     public static final int WIDTH = Resources.getSystem().getDisplayMetrics().widthPixels;
     public static final int HEIGHT = Resources.getSystem().getDisplayMetrics().heightPixels;
     private final int OPERAND_WIDTH = 246;
@@ -35,13 +36,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private RelativeLayout rLayout;
     private ArrayAdapter<CharSequence> adapter;
     private CustomImageView[] onCalculationList;
-    private int[] coordinatX;
     private ImageView trashZone;
+
+    private int[] coordinatX;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+        instances = this;
 
         //CONNECT ATTRIBUT KE VIEW
         this.trashZone = findViewById(R.id.trash_zone);
@@ -63,7 +66,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         this.bCalculate.setOnClickListener(this);
 
         //INISIALISASI ATTRIBUT
-        instances = this;
         this.onCalculationList = new CustomImageView[9];
         this.coordinatX = new int[9];
         for (int i = 0; i < coordinatX.length; i++) {
@@ -71,89 +73,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public void onClick(View view) {
 //        Log.d("MainActivity", "clicked");
         if (view == bAddOperand) {
             if (!etOperand.getText().toString().equalsIgnoreCase("")) {
                 CustomImageView newTv = createOp(etOperand.getText().toString(), 0);
-                newTv.setOnTouchListener(new CustomTouchListener());
+                newTv.setOnTouchListener(this);
                 rLayout.addView(newTv);
                 etOperand.setText("");
             }
         } else if (view == bAddOperator) {
             CustomImageView newTv = createOp(sOperator.getSelectedItem().toString(), 1);
-            newTv.setOnTouchListener(new CustomTouchListener());
+            newTv.setOnTouchListener(this);
             rLayout.addView(newTv);
         } else if (view == bCalculate) {
-            LinkedList<String> llOperand = new LinkedList<>();
-            LinkedList<String> llOperator = new LinkedList<>();
-            int jumlahOperand = 0;
-            int jumlahOperator = 0;
-            for(int i=0;i<onCalculationList.length;i++){
-                CustomImageView civ = onCalculationList[i];
-                if(i%2==0){
-                    if(civ == null){
-                        llOperand.add("NULL");
-                    }else{
-                        llOperand.add(civ.getText());
-                        jumlahOperand++;
-                    }
-                }else{
-                    if(civ == null){
-                        llOperator.add("NULL");
-                    }else{
-                        llOperator.add(civ.getText());
-                        jumlahOperator++;
-                    }
+            LinkedList<CustomImageView> llList = new LinkedList<>();
+            for (CustomImageView civ : onCalculationList) {
+                if (civ == null) {
+                    llList.add(null);
+                } else {
+                    llList.add(civ);
                 }
             }
-            if(jumlahOperand - jumlahOperator != 1){
-                //tidak valid
-            }else{
-                String a1 = llOperand.removeFirst();
-                String a2 = "";
-                String op = "";
-                String hasil = "";
-                boolean isValid = true;
-                while(!llOperand.isEmpty()){
-                    a2 = llOperand.removeFirst();
-                    op = llOperator.removeFirst();
-                    if(a1.equalsIgnoreCase("NULL") || a2.equalsIgnoreCase("NULL") || op.equalsIgnoreCase("NULL")){
-                        break;
-                    }
-                    hasil = Calculator.calculate(a1,a2,op)+"";
-                    a1 = hasil;
-                    a2 = "";
-                    op = "";
+            if (Calculator.getAnswer(llList)) {
+                CustomImageView civ = createOp((int) Double.parseDouble("" + Calculator.result) + "", 0);
+                civ.setOnTouchListener(this);
+                for (CustomImageView civ2 : onCalculationList) {
+                    remove(civ2);
+                    rLayout.removeView(civ2);
                 }
-                while(!llOperand.isEmpty()){
-                    String s = llOperand.removeFirst();
-                    if(!s.equalsIgnoreCase("NULL")){
-                        isValid = false;
-                    }
-                }
-                while(!llOperator.isEmpty()){
-                    String s = llOperator.removeFirst();
-                    if(!s.equalsIgnoreCase("NULL")){
-                        isValid = false;
-                    }
-                }
-                if(isValid) {
-                    Log.d("CALCULATE", hasil);
-                    for(CustomImageView civ : onCalculationList){
-                        if(civ != null) {
-                            this.remove(civ);
-                            ((ViewGroup) civ.getParent()).removeView(civ);
-                        }
-                    }
-                    CustomImageView newIV = this.createOp((int)Double.parseDouble(hasil)+"",0);
-                    newIV.setOnTouchListener(new CustomTouchListener());
-                    rLayout.addView(newIV);
-                }else{
-                    Toast toast = Toast.makeText(getApplicationContext(),"Data tidak valid",Toast.LENGTH_LONG);
-                    toast.show();
-                }
+                rLayout.addView(civ);
+//                Log.d("CALCULATE",""+Calculator.result);
+            } else {
+                Toast toast = Toast.makeText(this, "Data tidak valid", Toast.LENGTH_LONG);
+                toast.show();
+//                Log.d("CALCULATE", "TIDAK VALID");
             }
         }
     }
@@ -206,8 +162,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         paint.setColor(getResources().getColor(R.color.black));
         paint.setAntiAlias(true);
         canvas.drawText(text, x, y, paint);
-        float randX = (float) Math.random() * (WIDTH - 400) + 200;
-        float randY = (float) (Math.random()) * (HEIGHT - 600) + 200;
+        float randX = (float) Math.random() * (WIDTH - 600) + 300;
+        float randY = (float) (Math.random()) * (HEIGHT - 1000) + 400;
         tv.setX(randX);
         tv.setY(randY);
         tv.invalidate();
@@ -326,5 +282,110 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     public boolean isKosong(int idx) {
         return onCalculationList[idx] == null;
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        switch (motionEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_UP:
+                //kalau di area atas
+                if (view.getY() <= 250) {
+                    //kalau view sudah ada di slot
+                    if (MainActivity.getInstances().contains(view)) {
+                        int idx = MainActivity.getInstances().getIdxTerdekat(view);
+                        //kalau slot baru masih kosong
+                        if (MainActivity.getInstances().isKosong(idx)) {
+                            MainActivity.getInstances().remove(view);
+                            MainActivity.getInstances().add((CustomImageView) view);
+                        }
+                        //kalau slot baru tidak kosong
+                        else {
+                            float tempX = (float) Math.random() * (MainActivity.WIDTH - 400) + 200;
+                            float tempY = (float) (Math.random()) * (MainActivity.HEIGHT - 600) + 200;
+                            view.setX(tempX);
+                            view.setY(tempY);
+                            MainActivity.getInstances().remove(view);
+                        }
+                    }
+                    //kalau view belum di slot
+                    else {
+                        //kalau slot baru tidak kosong
+                        if (!MainActivity.getInstances().isKosong(MainActivity.getInstances().getIdxTerdekat(view))) {
+                            float tempX = (float) Math.random() * (MainActivity.WIDTH - 400) + 200;
+                            float tempY = (float) (Math.random()) * (MainActivity.HEIGHT - 600) + 200;
+                            view.setX(tempX);
+                            view.setY(tempY);
+                        }
+                        //kalau slot baru kosong
+                        else {
+                            MainActivity.getInstances().add((CustomImageView) view);
+                        }
+                    }
+                }
+                //kalau bukan di area atas
+                else {
+                    //kalau view udah ada di array
+                    if (MainActivity.getInstances().contains(view)) {
+                        MainActivity.getInstances().remove(view);
+                    }
+                    //kalau view belum ada di array
+                    else {
+                        //do nothing
+                    }
+                }
+
+                if (view.getX() <= 0) {
+                    view.setX(1);
+                }
+                if (view.getX() + view.getWidth() >= MainActivity.WIDTH) {
+                    view.setX(MainActivity.WIDTH - view.getWidth() - 1);
+                }
+                if (view.getY() <= 0) {
+                    view.setY(1);
+                }
+                if (view.getY() + view.getHeight() >= MainActivity.HEIGHT) {
+                    view.setY(MainActivity.HEIGHT - view.getHeight() - 1);
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+//                Log.d("CALCULATOR",trashZone.getX()+" "+trashZone.getWidth()+" "+trashZone.getY()+" "+trashZone.getHeight());
+//                Log.d("CALCULATOR",view.getX()+(view.getWidth()/2)+" "+view.getY()+(view.getHeight()/2));
+//                Log.d("CALCULATOR",
+//                        "getX > tzGetX: "+(view.getX()+(view.getWidth()/2) > trashZone.getX())
+//                                +"\ngetX<tzGetX+width"+(view.getX()+(view.getWidth()/2) < trashZone.getX()+trashZone.getWidth())
+//                                +"\ngetY>tzGetY: "+(view.getY()+(view.getHeight()/2) > trashZone.getY())
+//                                +"\ngetY<tzGetY+height: "+(view.getY()+(view.getHeight()/2) < trashZone.getY()+trashZone.getHeight()));
+
+                if (view.getX() + (view.getWidth() / 2) > MainActivity.getInstances().getTrashZone().getX() && view.getX() + (view.getWidth() / 2) < (MainActivity.getInstances().getTrashZone().getX() + MainActivity.getInstances().getTrashZone().getWidth()) &&
+                        view.getY() + (view.getHeight() / 2) > MainActivity.getInstances().getTrashZone().getY() && view.getY() + (view.getHeight() / 2) < (MainActivity.getInstances().getTrashZone().getY() + MainActivity.getInstances().getTrashZone().getHeight())) {
+//                    Log.d("CALCULATOR-TRASH_ZONE","MASUK");
+                    if (MainActivity.getInstances().contains(view)) {
+                        MainActivity.getInstances().remove(view);
+                    }
+                    ((ViewGroup) view.getParent()).removeView(view);
+                }
+                //JARI MENGGERAKAN VIEW DI BAGIAN TENGAH VIEW SEBAGAI TITIK PUSATNYA
+                if (view.getX() > 0 && view.getX() + view.getWidth() < MainActivity.WIDTH && view.getY() > 0 && view.getY() < MainActivity.HEIGHT) {
+                    view.setX(motionEvent.getRawX() - view.getWidth() / 2.0f);
+                    view.setY(motionEvent.getRawY() - view.getHeight());
+                } else {
+                    if (view.getX() <= 0) {
+                        view.setX(1);
+                    }
+                    if (view.getX() + view.getWidth() >= MainActivity.WIDTH) {
+                        view.setX(MainActivity.WIDTH - view.getWidth() - 1);
+                    }
+                    if (view.getY() <= 0) {
+                        view.setY(1);
+                    }
+                    if (view.getY() + view.getHeight() >= MainActivity.HEIGHT) {
+                        view.setY(MainActivity.HEIGHT - view.getHeight() - 1);
+                    }
+                }
+                break;
+        }
+        return true;
     }
 }
