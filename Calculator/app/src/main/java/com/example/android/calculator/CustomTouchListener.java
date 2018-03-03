42f34ba546af40515aca113aba1ed0552372da2f
vgnaruto <caurnt@gmail.com>
2018-03-03 15:46:00 +0700

    Layout

--- Calculator/app/src/main/java/com/example/android/calculator/CustomTouchListener.java
+++ Calculator/app/src/main/java/com/example/android/calculator/CustomTouchListener.java
@@ -11,72 +11,73 @@ import android.view.ViewGroup;
  */
 
 public class CustomTouchListener implements View.OnTouchListener {
+
     @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            switch (motionEvent.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    break;
-                case MotionEvent.ACTION_UP:
-                    //kalau di area atas
-                    if (view.getY() <= 250) {
-                        //kalau view sudah ada di slot
-                        if (MainActivity.getInstances().contains(view)) {
-                            int idx = MainActivity.getInstances().getIdxTerdekat(view);
-                            //kalau slot baru masih kosong
-                            if(MainActivity.getInstances().isKosong(idx)){
-                                MainActivity.getInstances().remove(view);
-                                MainActivity.getInstances().add((CustomImageView) view);
-                            }
-                            //kalau slot baru tidak kosong
-                            else{
-                                float tempX = (float) Math.random() * (MainActivity.WIDTH - 400) + 200;
-                                float tempY = (float) (Math.random()) * (MainActivity.HEIGHT - 600) + 200;
-                                view.setX(tempX);
-                                view.setY(tempY);
-                                MainActivity.getInstances().remove(view);
-                            }
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        switch (motionEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_UP:
+                //kalau di area atas
+                if (view.getY() <= 250) {
+                    //kalau view sudah ada di slot
+                    if (MainActivity.getInstances().contains(view)) {
+                        int idx = MainActivity.getInstances().getIdxTerdekat(view);
+                        //kalau slot baru masih kosong
+                        if (MainActivity.getInstances().isKosong(idx)) {
+                            MainActivity.getInstances().remove(view);
+                            MainActivity.getInstances().add((CustomImageView) view);
                         }
-                        //kalau view belum di slot
+                        //kalau slot baru tidak kosong
                         else {
-                            //kalau slot baru tidak kosong
-                            if (!MainActivity.getInstances().isKosong(MainActivity.getInstances().getIdxTerdekat(view))) {
-                                float tempX = (float) Math.random() * (MainActivity.WIDTH - 400) + 200;
-                                float tempY = (float) (Math.random()) * (MainActivity.HEIGHT - 600) + 200;
-                                view.setX(tempX);
-                                view.setY(tempY);
-                            }
-                            //kalau slot baru kosong
-                            else {
-                                MainActivity.getInstances().add((CustomImageView) view);
-                            }
-                        }
-                    }
-                    //kalau bukan di area atas
-                    else {
-                        //kalau view udah ada di array
-                        if (MainActivity.getInstances().contains(view)) {
+                            float tempX = (float) Math.random() * (MainActivity.WIDTH - 400) + 200;
+                            float tempY = (float) (Math.random()) * (MainActivity.HEIGHT - 600) + 200;
+                            view.setX(tempX);
+                            view.setY(tempY);
                             MainActivity.getInstances().remove(view);
                         }
-                        //kalau view belum ada di array
+                    }
+                    //kalau view belum di slot
+                    else {
+                        //kalau slot baru tidak kosong
+                        if (!MainActivity.getInstances().isKosong(MainActivity.getInstances().getIdxTerdekat(view))) {
+                            float tempX = (float) Math.random() * (MainActivity.WIDTH - 400) + 200;
+                            float tempY = (float) (Math.random()) * (MainActivity.HEIGHT - 600) + 200;
+                            view.setX(tempX);
+                            view.setY(tempY);
+                        }
+                        //kalau slot baru kosong
                         else {
-                            //do nothing
+                            MainActivity.getInstances().add((CustomImageView) view);
                         }
                     }
+                }
+                //kalau bukan di area atas
+                else {
+                    //kalau view udah ada di array
+                    if (MainActivity.getInstances().contains(view)) {
+                        MainActivity.getInstances().remove(view);
+                    }
+                    //kalau view belum ada di array
+                    else {
+                        //do nothing
+                    }
+                }
 
-                    if(view.getX() <= 0){
-                        view.setX(1);
-                    }
-                    if(view.getX()+view.getWidth() >= MainActivity.WIDTH){
-                        view.setX(MainActivity.WIDTH-view.getWidth()-1);
-                    }
-                    if(view.getY() <= 0){
-                        view.setY(1);
-                    }
-                    if(view.getY()+view.getHeight() >= MainActivity.HEIGHT){
-                        view.setY(MainActivity.HEIGHT-view.getHeight()-1);
-                    }
-                    break;
-                case MotionEvent.ACTION_MOVE:
+                if (view.getX() <= 0) {
+                    view.setX(1);
+                }
+                if (view.getX() + view.getWidth() >= MainActivity.WIDTH) {
+                    view.setX(MainActivity.WIDTH - view.getWidth() - 1);
+                }
+                if (view.getY() <= 0) {
+                    view.setY(1);
+                }
+                if (view.getY() + view.getHeight() >= MainActivity.HEIGHT) {
+                    view.setY(MainActivity.HEIGHT - view.getHeight() - 1);
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
 //                Log.d("CALCULATOR",trashZone.getX()+" "+trashZone.getWidth()+" "+trashZone.getY()+" "+trashZone.getHeight());
 //                Log.d("CALCULATOR",view.getX()+(view.getWidth()/2)+" "+view.getY()+(view.getHeight()/2));
 //                Log.d("CALCULATOR",
@@ -85,34 +86,35 @@ public class CustomTouchListener implements View.OnTouchListener {
 //                                +"\ngetY>tzGetY: "+(view.getY()+(view.getHeight()/2) > trashZone.getY())
 //                                +"\ngetY<tzGetY+height: "+(view.getY()+(view.getHeight()/2) < trashZone.getY()+trashZone.getHeight()));
 
-                    if (view.getX() + (view.getWidth() / 2) > MainActivity.getInstances().getTrashZone().getX() && view.getX() + (view.getWidth() / 2) < (MainActivity.getInstances().getTrashZone().getX() + MainActivity.getInstances().getTrashZone().getWidth()) &&
-                            view.getY() + (view.getHeight() / 2) > MainActivity.getInstances().getTrashZone().getY() && view.getY() + (view.getHeight() / 2) < (MainActivity.getInstances().getTrashZone().getY() + MainActivity.getInstances().getTrashZone().getHeight())) {
+                if (view.getX() + (view.getWidth() / 2) > MainActivity.getInstances().getTrashZone().getX() && view.getX() + (view.getWidth() / 2) < (MainActivity.getInstances().getTrashZone().getX() + MainActivity.getInstances().getTrashZone().getWidth()) &&
+                        view.getY() + (view.getHeight() / 2) > MainActivity.getInstances().getTrashZone().getY() && view.getY() + (view.getHeight() / 2) < (MainActivity.getInstances().getTrashZone().getY() + MainActivity.getInstances().getTrashZone().getHeight())) {
 //                    Log.d("CALCULATOR-TRASH_ZONE","MASUK");
-                        if (MainActivity.getInstances().contains(view)) {
-                            MainActivity.getInstances().remove(view);
-                        }
-                        ((ViewGroup) view.getParent()).removeView(view);
+                    if (MainActivity.getInstances().contains(view)) {
+                        MainActivity.getInstances().remove(view);
                     }
-                    //JARI MENGGERAKAN VIEW DI BAGIAN TENGAH VIEW SEBAGAI TITIK PUSATNYA
-                    if(view.getX() > 0 && view.getX()+view.getWidth() < MainActivity.WIDTH && view.getY() > 0 && view.getY() < MainActivity.HEIGHT) {
-                        view.setX(motionEvent.getRawX() - view.getWidth() / 2.0f);
-                        view.setY(motionEvent.getRawY() - view.getHeight());
-                    }else{
-                        if(view.getX() <= 0){
-                            view.setX(1);
-                        }
-                        if(view.getX()+view.getWidth() >= MainActivity.WIDTH){
-                            view.setX(MainActivity.WIDTH-view.getWidth()-1);
-                        }
-                        if(view.getY() <= 0){
-                            view.setY(1);
-                        }
-                        if(view.getY()+view.getHeight() >= MainActivity.HEIGHT){
-                            view.setY(MainActivity.HEIGHT-view.getHeight()-1);
-                        }
+                    ((ViewGroup) view.getParent()).removeView(view);
+                }
+                //JARI MENGGERAKAN VIEW DI BAGIAN TENGAH VIEW SEBAGAI TITIK PUSATNYA
+                if (view.getX() > 0 && view.getX() + view.getWidth() < MainActivity.WIDTH && view.getY() > 0 && view.getY() < MainActivity.HEIGHT) {
+                    view.setX(motionEvent.getRawX() - view.getWidth() / 2.0f);
+                    view.setY(motionEvent.getRawY() - view.getHeight());
+                } else {
+                    if (view.getX() <= 0) {
+                        view.setX(1);
                     }
-                    break;
-            }
-            return true;
+                    if (view.getX() + view.getWidth() >= MainActivity.WIDTH) {
+                        view.setX(MainActivity.WIDTH - view.getWidth() - 1);
+                    }
+                    if (view.getY() <= 0) {
+                        view.setY(1);
+                    }
+                    if (view.getY() + view.getHeight() >= MainActivity.HEIGHT) {
+                        view.setY(MainActivity.HEIGHT - view.getHeight() - 1);
+                    }
+                }
+                break;
+        }
+
+        return true;
     }
 }
--- Calculator/app/src/main/java/com/example/android/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/android/calculator/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -37,6 +38,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ArrayAdapter<CharSequence> adapter;
     private CustomImageView[] onCalculationList;
     private ImageView trashZone;
+
+    private GestureDetector mGD;
 
     private int[] coordinatX;
 
@@ -64,9 +67,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         this.bAddOperand.setOnClickListener(this);
         this.bAddOperator.setOnClickListener(this);
         this.bCalculate.setOnClickListener(this);
+        this.sOperator.setOnTouchListener(this);
+        this.etOperand.setOnTouchListener(this);
 
         //INISIALISASI ATTRIBUT
         this.onCalculationList = new CustomImageView[9];
+        this.mGD = new GestureDetector(this,new CustomGestureDetector());
         this.coordinatX = new int[9];
         for (int i = 0; i < coordinatX.length; i++) {
             coordinatX[i] = posisiX(i);
@@ -112,11 +118,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //                Log.d("CALCULATE", "TIDAK VALID");
             }
         }
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        return super.onTouchEvent(event);
     }
 
     @SuppressLint("NewApi")
@@ -286,91 +287,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
-        switch (motionEvent.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                break;
-            case MotionEvent.ACTION_UP:
-                //kalau di area atas
-                if (view.getY() <= 250) {
-                    //kalau view sudah ada di slot
-                    if (MainActivity.getInstances().contains(view)) {
-                        int idx = MainActivity.getInstances().getIdxTerdekat(view);
-                        //kalau slot baru masih kosong
-                        if (MainActivity.getInstances().isKosong(idx)) {
-                            MainActivity.getInstances().remove(view);
-                            MainActivity.getInstances().add((CustomImageView) view);
+        if(view != this.etOperand && view != this.sOperator) {
+            switch (motionEvent.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    break;
+                case MotionEvent.ACTION_UP:
+                    //kalau di area atas
+                    if (view.getY() <= 250) {
+                        //kalau view sudah ada di slot
+                        if (MainActivity.getInstances().contains(view)) {
+                            int idx = MainActivity.getInstances().getIdxTerdekat(view);
+                            //kalau slot baru masih kosong
+                            if (MainActivity.getInstances().isKosong(idx)) {
+                                MainActivity.getInstances().remove(view);
+                                MainActivity.getInstances().add((CustomImageView) view);
+                            }
+                            //kalau slot baru tidak kosong
+                            else {
+                                float tempX = (float) Math.random() * (MainActivity.WIDTH - 400) + 200;
+                                float tempY = (float) (Math.random()) * (MainActivity.HEIGHT - 600) + 200;
+                                view.setX(tempX);
+                                view.setY(tempY);
+                                MainActivity.getInstances().remove(view);
+                            }
                         }
-                        //kalau slot baru tidak kosong
+                        //kalau view belum di slot
                         else {
-                            float tempX = (float) Math.random() * (MainActivity.WIDTH - 400) + 200;
-                            float tempY = (float) (Math.random()) * (MainActivity.HEIGHT - 600) + 200;
-                            view.setX(tempX);
-                            view.setY(tempY);
+                            //kalau slot baru tidak kosong
+                            if (!MainActivity.getInstances().isKosong(MainActivity.getInstances().getIdxTerdekat(view))) {
+                                float tempX = (float) Math.random() * (MainActivity.WIDTH - 400) + 200;
+                                float tempY = (float) (Math.random()) * (MainActivity.HEIGHT - 600) + 200;
+                                view.setX(tempX);
+                                view.setY(tempY);
+                            }
+                            //kalau slot baru kosong
+                            else {
+                                MainActivity.getInstances().add((CustomImageView) view);
+                            }
+                        }
+                    }
+                    //kalau bukan di area atas
+                    else {
+                        //kalau view udah ada di array
+                        if (MainActivity.getInstances().contains(view)) {
                             MainActivity.getInstances().remove(view);
                         }
-                    }
-                    //kalau view belum di slot
-                    else {
-                        //kalau slot baru tidak kosong
-                        if (!MainActivity.getInstances().isKosong(MainActivity.getInstances().getIdxTerdekat(view))) {
-                            float tempX = (float) Math.random() * (MainActivity.WIDTH - 400) + 200;
-                            float tempY = (float) (Math.random()) * (MainActivity.HEIGHT - 600) + 200;
-                            view.setX(tempX);
-                            view.setY(tempY);
-                        }
-                        //kalau slot baru kosong
+                        //kalau view belum ada di array
                         else {
-                            MainActivity.getInstances().add((CustomImageView) view);
+                            //do nothing
                         }
                     }
-                }
-                //kalau bukan di area atas
-                else {
-                    //kalau view udah ada di array
-                    if (MainActivity.getInstances().contains(view)) {
-                        MainActivity.getInstances().remove(view);
-                    }
-                    //kalau view belum ada di array
-                    else {
-                        //do nothing
-                    }
-                }
 
-                if (view.getX() <= 0) {
-                    view.setX(1);
-                }
-                if (view.getX() + view.getWidth() >= MainActivity.WIDTH) {
-                    view.setX(MainActivity.WIDTH - view.getWidth() - 1);
-                }
-                if (view.getY() <= 0) {
-                    view.setY(1);
-                }
-                if (view.getY() + view.getHeight() >= MainActivity.HEIGHT) {
-                    view.setY(MainActivity.HEIGHT - view.getHeight() - 1);
-                }
-                break;
-            case MotionEvent.ACTION_MOVE:
-//                Log.d("CALCULATOR",trashZone.getX()+" "+trashZone.getWidth()+" "+trashZone.getY()+" "+trashZone.getHeight());
-//                Log.d("CALCULATOR",view.getX()+(view.getWidth()/2)+" "+view.getY()+(view.getHeight()/2));
-//                Log.d("CALCULATOR",
-//                        "getX > tzGetX: "+(view.getX()+(view.getWidth()/2) > trashZone.getX())
-//                                +"\ngetX<tzGetX+width"+(view.getX()+(view.getWidth()/2) < trashZone.getX()+trashZone.getWidth())
-//                                +"\ngetY>tzGetY: "+(view.getY()+(view.getHeight()/2) > trashZone.getY())
-//                                +"\ngetY<tzGetY+height: "+(view.getY()+(view.getHeight()/2) < trashZone.getY()+trashZone.getHeight()));
-
-                if (view.getX() + (view.getWidth() / 2) > MainActivity.getInstances().getTrashZone().getX() && view.getX() + (view.getWidth() / 2) < (MainActivity.getInstances().getTrashZone().getX() + MainActivity.getInstances().getTrashZone().getWidth()) &&
-                        view.getY() + (view.getHeight() / 2) > MainActivity.getInstances().getTrashZone().getY() && view.getY() + (view.getHeight() / 2) < (MainActivity.getInstances().getTrashZone().getY() + MainActivity.getInstances().getTrashZone().getHeight())) {
-//                    Log.d("CALCULATOR-TRASH_ZONE","MASUK");
-                    if (MainActivity.getInstances().contains(view)) {
-                        MainActivity.getInstances().remove(view);
-                    }
-                    ((ViewGroup) view.getParent()).removeView(view);
-                }
-                //JARI MENGGERAKAN VIEW DI BAGIAN TENGAH VIEW SEBAGAI TITIK PUSATNYA
-                if (view.getX() > 0 && view.getX() + view.getWidth() < MainActivity.WIDTH && view.getY() > 0 && view.getY() < MainActivity.HEIGHT) {
-                    view.setX(motionEvent.getRawX() - view.getWidth() / 2.0f);
-                    view.setY(motionEvent.getRawY() - view.getHeight());
-                } else {
                     if (view.getX() <= 0) {
                         view.setX(1);
                     }
@@ -383,9 +350,89 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     if (view.getY() + view.getHeight() >= MainActivity.HEIGHT) {
                         view.setY(MainActivity.HEIGHT - view.getHeight() - 1);
                     }
-                }
-                break;
+                    break;
+                case MotionEvent.ACTION_MOVE:
+//                Log.d("CALCULATOR",trashZone.getX()+" "+trashZone.getWidth()+" "+trashZone.getY()+" "+trashZone.getHeight());
+//                Log.d("CALCULATOR",view.getX()+(view.getWidth()/2)+" "+view.getY()+(view.getHeight()/2));
+//                Log.d("CALCULATOR",
+//                        "getX > tzGetX: "+(view.getX()+(view.getWidth()/2) > trashZone.getX())
+//                                +"\ngetX<tzGetX+width"+(view.getX()+(view.getWidth()/2) < trashZone.getX()+trashZone.getWidth())
+//                                +"\ngetY>tzGetY: "+(view.getY()+(view.getHeight()/2) > trashZone.getY())
+//                                +"\ngetY<tzGetY+height: "+(view.getY()+(view.getHeight()/2) < trashZone.getY()+trashZone.getHeight()));
+
+                    if (view.getX() + (view.getWidth() / 2) > MainActivity.getInstances().getTrashZone().getX() && view.getX() + (view.getWidth() / 2) < (MainActivity.getInstances().getTrashZone().getX() + MainActivity.getInstances().getTrashZone().getWidth()) &&
+                            view.getY() + (view.getHeight() / 2) > MainActivity.getInstances().getTrashZone().getY() && view.getY() + (view.getHeight() / 2) < (MainActivity.getInstances().getTrashZone().getY() + MainActivity.getInstances().getTrashZone().getHeight())) {
+//                    Log.d("CALCULATOR-TRASH_ZONE","MASUK");
+                        if (MainActivity.getInstances().contains(view)) {
+                            MainActivity.getInstances().remove(view);
+                        }
+                        ((ViewGroup) view.getParent()).removeView(view);
+                    }
+                    //JARI MENGGERAKAN VIEW DI BAGIAN TENGAH VIEW SEBAGAI TITIK PUSATNYA
+                    if (view.getX() > 0 && view.getX() + view.getWidth() < MainActivity.WIDTH && view.getY() > 0 && view.getY() < MainActivity.HEIGHT) {
+                        view.setX(motionEvent.getRawX() - view.getWidth() / 2.0f);
+                        view.setY(motionEvent.getRawY() - view.getHeight());
+                    } else {
+                        if (view.getX() <= 0) {
+                            view.setX(1);
+                        }
+                        if (view.getX() + view.getWidth() >= MainActivity.WIDTH) {
+                            view.setX(MainActivity.WIDTH - view.getWidth() - 1);
+                        }
+                        if (view.getY() <= 0) {
+                            view.setY(1);
+                        }
+                        if (view.getY() + view.getHeight() >= MainActivity.HEIGHT) {
+                            view.setY(MainActivity.HEIGHT - view.getHeight() - 1);
+                        }
+                    }
+                    break;
+            }
+        }else if(view == sOperator || view == etOperand){
+            return mGD.onTouchEvent(motionEvent);
         }
         return true;
+    }
+
+    public EditText getEditText(){
+        return this.etOperand;
+    }
+    public Spinner getSpinner(){
+        return this.sOperator;
+    }
+
+    private class CustomGestureDetector extends GestureDetector.SimpleOnGestureListener{
+        @Override
+        public void onLongPress(MotionEvent e) {
+//            Log.d("LONGPRESS",e.getRawX()+" "+e.getRawY()+
+//                    "\n"+etOperand.getX()+" "+(etOperand.getX()+etOperand.getWidth())+
+//                    "\n"+etOperand.getY()+" "+(etOperand.getY()+etOperand.getHeight()));
+//            Log.d("LONGPRESS",e.getRawX()+" "+e.getRawY()+
+//                    "\n"+sOperator.getX()+" "+(sOperator.getX()+sOperator.getWidth())+
+//                    "\n"+sOperator.getY()+" "+(sOperator.getY()+sOperator.getHeight()));
+            //EDIT TEXT OPERAND
+            if(e.getRawX() >= etOperand.getX() && e.getRawX() <= etOperand.getX() + etOperand.getWidth()){
+                if((e.getRawY()-100) >= etOperand.getY() && (e.getRawY()-100) <= etOperand.getY() + etOperand.getHeight()){
+                    if(!etOperand.getText().toString().equalsIgnoreCase("")) {
+                        CustomImageView newTv = createOp(etOperand.getText().toString(), 0);
+                        newTv.setX(e.getRawX());
+                        newTv.setY(e.getRawY()-100);
+                        newTv.setOnTouchListener(MainActivity.getInstances());
+                        rLayout.addView(newTv);
+                        etOperand.setText("");
+                    }
+                }
+            }
+            //SPINNER
+            else if(e.getRawX() >= sOperator.getX() && e.getRawX() <= sOperator.getX() + sOperator.getWidth()){
+                if((e.getRawY()-75) >= sOperator.getY() && (e.getRawY()-75) <= sOperator.getY() + sOperator.getHeight()){
+                    CustomImageView newTv = createOp(sOperator.getSelectedItem().toString(), 1);
+                    newTv.setX(e.getRawX());
+                    newTv.setY(e.getRawY()-75);
+                    newTv.setOnTouchListener(MainActivity.getInstances());
+                    rLayout.addView(newTv);
+                }
+            }
+        }
     }
 }
